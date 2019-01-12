985889da17577619de5bcc68e95519e57c2683de
Nelson Daniel Troncoso <nd.ta@hotmail.com>
2019-01-11 20:02:25 -0800

    file comparison for expected output - wip

--- .scripts/run.ps1
+++ .scripts/run.ps1
@@ -5,8 +5,6 @@ $ErrorActionPreference  = "stop"
 # This script requires the following:
 # git
 
-
-
 ## ===========================================================================
 # script
   
@@ -20,26 +18,50 @@ $allreadmes = get-childitem $restSpecs\readme.md -recurse  | where { $_.FullName
 
 $x =0 
 
-$allreadmes |% {
-  if( $x -lt 20) {
+// get directory names of new generated files that already were correctly generated
+$allpassed = (get-childitem -path $expected_files_dir).Name
+# wrap in a list if there is just one element
+if ($allpassed -is [String]) {
+  $allpassed = @($allpassed)
+}
+
+// passed files: 
+$allpassed
+
+$allreadmes | ForEach-Object {
+  if( $x -lt 200) {
     $file = $_
 
-    
-    autorest-orig $out_1/$x/cs $file --csharp.output-folder:$out_1/$x/cs  
-    autorest-orig $out_1/$x/py $file --python.output-folder:$out_1/$x/py
-    autorest-orig $out_1/$x/ts $file --typescript.output-folder:$out_1/$x/ts
-    autorest-orig $out_1/$x/java $file --java.output-folder:$out_1/$x/java
-    autorest-orig $out_1/$x/ruby $file --ruby.output-folder:$out_1/$x/ruby
-    autorest-orig $out_1/$x/ars $file --azureresourceschema.output-folder:$out_1/$x/ars
-
+    # always generate the new files
     autorest-new $out_2/$x/cs $file --csharp.output-folder:$out_2/$x/cs
     autorest-new $out_2/$x/py $file --python.output-folder:$out_2/$x/py
     autorest-new $out_2/$x/ts $file --typescript.output-folder:$out_2/$x/ts
     autorest-new $out_2/$x/java $file --java.output-folder:$out_2/$x/java
-    autorest-new $out_2/$x/ruby $file --ruby.output-folder:$out_2/$x/ruby
+    # autorest-new $out_2/$x/ruby $file --ruby.output-folder:$out_2/$x/ruby
     autorest-new $out_2/$x/ars $file --azureresourceschema.output-folder:$out_2/$x/ars
 
+    if ($allpassed -and $allpassed.Contains($x.ToString())) {
+      $actualFiles =  Get-ChildItem –Path $out_2/$x | ForEach-Object  {Get-FileHash –Path $_.FullName}
+      $expectedFiles = Get-ChildItem –Path $expected_files_dir -Filter "*.txt" | ForEach-Object  {Get-FileHash –Path $_.FullName
+      $failPath = (Compare-Object -ReferenceObject $actualFiles -DifferenceObject $expectedFiles  -Property hash -PassThru).Path
+      if ($failPath) {
+        // Passed $x
+      } else {
+        /! Failed: $failPath
+      }
+    } else {
+      # if there are not expected files yet, produce the originals to compare
+
+      autorest-orig $out_1/$x/cs $file --csharp.output-folder:$out_1/$x/cs  
+      autorest-orig $out_1/$x/py $file --python.output-folder:$out_1/$x/py
+      autorest-orig $out_1/$x/ts $file --typescript.output-folder:$out_1/$x/ts
+      autorest-orig $out_1/$x/java $file --java.output-folder:$out_1/$x/java
+      # autorest-orig $out_1/$x/ruby $file --ruby.output-folder:$out_1/$x/ruby
+      autorest-orig $out_1/$x/ars $file --azureresourceschema.output-folder:$out_1/$x/ars
+    }
+
   }
+
   $x = $x + 1  
 
   return;
